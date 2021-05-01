5f6d3825cdc95f0d293d8752bbe1a19cf697ca46
Yusuf <y.sertbolat@gmail.com>
2021-05-01 05:36:19 +0300

    Initial commit

---
+++ EntityFrameworkDemo/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ProductDal _productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+
+        private void SearchProducts(string key) //arama kısmını oluşturmak için
+        {
+            //var result = _productDal.GetAll().Where(p=>p.Name.ToLower().Contains(key.ToLower())).ToList(); //burada veri tabanını sorgulayıp listeye geçiş yapmışız
+            //where = nerden yararlanacağımız p = her bir elemanın p.Name'i kapsar(key)'i .ToList = result olarak liste şeklinde göstermesi için
+            
+            var result = _productDal.GetByName(key); //burada productDal kısmında yaptığımız search ile ilgili (LINQ) kısma sorguyu göndermiş olduk ve büyük küçük harfleri de kapsayan bir arama sistemi kurmuş olduk
+            dgwProducts.DataSource = result;  
+
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = tbxName.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmount.Text)
+            });
+            LoadProducts();
+            MessageBox.Show("Added!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            _productDal.Update(new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value),
+                Name = tbxNameUpdate.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPriceUpdate.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmountUpdate.Text)
+            });
+            LoadProducts();
+            MessageBox.Show("Updated!");
+        }
+
+        private void dgwProducts_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxNameUpdate.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString(); 
+            //dpwproducts'ın seçili olan satırının hücrelerinden birincisinin değerini stringe çevir demek
+            tbxUnitPriceUpdate.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            tbxStockAmountUpdate.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString(); //update kısmına yazmış olduk
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            _productDal.Delete(new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value)
+            });
+            MessageBox.Show("Deleted!");
+        }
+
+        private void tbxSearch_TextChanged(object sender, EventArgs e)
+        {
+            SearchProducts(tbxSearch.Text);
+        }
+
+        private void tbxGetById_Click(object sender, EventArgs e)
+        {
+            _productDal.GetById(3); //eğer buraya hangi id'yi yollarsak productDal'ın alt tabanına onun bilgilerini yollar eğer öyle bir id yoksa "null" yazar
+        }
+    }
+}
---
+++ EntityFrameworkDemo/ProductDal.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class ProductDal
+    {
+        internal List<Product> GetAll()
+        {
+            using (ETradeContext context = new ETradeContext()) //bu belllekte çok fazla yer kaplar f12 ile IDisponsable kısmına ulaşabilirsiniz using ise çöp toplayıcılar kodu bitirmeden nesneyi zorla bellekten atıyosunuz az yer kaplamanızı sağlıyor (Dispoble etmek)
+            {
+                return context.Products.ToList(); //form1de yazdığımız kodu kesip buraya yapıştırdık ve sadece burayı değiştirdik
+            }
+        }
+
+        internal List<Product> GetByName(string key) //veri tabanına sormak için 
+        {
+            using (ETradeContext context = new ETradeContext()) 
+            {
+                return context.Products.Where(p=>p.Name.Contains(key)).ToList(); //burada direkt veri tabanıyla işimiz bitmeden ürüne ulaşmışız (çok veri olduğunda daha verimli bir yöntem)
+            }
+        }
+
+        internal List<Product> GetByUnitPrice(decimal price) //fiyat olarak aramak istersek 
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.Where(p => p.UnitPrice >= price).ToList();
+                //burada veri tabanını sorgulayıp ürünleri filtreleyip fiyat sorgulamak için bir kod yazdık
+            }
+        }
+        internal List<Product> GetByUnitPrice(decimal min, decimal max) //iki fiyat arası için filtreleme
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.Where(p => p.UnitPrice >= min && p.UnitPrice <= max).ToList();
+                //burada veri tabanını sorgulayıp ürünleri filtreleyip iki fiyat arasındaki verileri almak için kod yazdık
+            }
+        }
+        internal Product GetById(int id) //tek bir ürünü aramak için
+        {
+            using (ETradeContext context = new ETradeContext()) 
+            {
+               var result = context.Products.FirstOrDefault(p => p.Id == id);
+                //FirstOrDefault= id'ye uygun olan ilk kaydı veya null getir yazdık ardından parametre içine p için p'nin idsi == kullanıcının gönderdiği id anlamında bir komut yazdık
+                return result;
+            }
+        }
+
+        internal void Add(Product product) //add ve get all daha erişilebilir olduğu için internal yapmak zorunda kaldım
+        {
+            using (ETradeContext context = new ETradeContext()) 
+            {
+                //context.Products.Add(product); bu da olur
+                var entity = context.Entry(product); //bu da olur
+                //context'e product için abone olup eşitle
+                entity.State = EntityState.Added;
+                //durumuna da ekle demek
+                context.SaveChanges();
+            }
+        }
+        internal void Update(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                //context'e product için abone olup eşitle
+                entity.State = EntityState.Modified;
+                //durumunu da güncelle demek
+                context.SaveChanges();
+            }
+        }
+
+        internal void Delete(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                //context'e product için abone olup eşitle
+                entity.State = EntityState.Deleted;
+                //durumunu da sil demek
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
